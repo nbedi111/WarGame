683ec3644181dd42aa7f05cb478817a4edce32b8
nbedi111 <141073828+nbedi111@users.noreply.github.com>
2023-07-31 17:03:01 -0400

    Add files via upload

---
+++ War Game/War Game/War_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_GameApp.swift
+//  War Game
+//
+//  Created by Nayana Sharma on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct War_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
